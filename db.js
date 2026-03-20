495c6f50111c85a386e226509507b4e5812ca530
Chethana-Gopalakrishna <gopalakrishnachethana@gmail.com>
2026-03-20 23:25:52 +0530

    db.js

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
-const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
-const mariadb = require("mariadb");
 
-const connectionString = process.env.DATABASE_URL.replace("mysql://", "mariadb://");
-const pool = mariadb.createPool(connectionString);
-const adapter = new PrismaMariaDb(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = { prisma };
